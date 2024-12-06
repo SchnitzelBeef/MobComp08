205cae31df706433106341083c9477b193bb9244
rkaundal00 <rahulkaundal00@hotmail.com>
2024-12-06 14:36:55 +0100

    Forgot to add PIDcontroller2.java file

---
+++ PIDController2.java
@@ -0,0 +1,41 @@
+// package layer2_802Algorithms;
+
+// **ADDED**
+public final class PIDController {
+
+	private double commulating_error = 0;
+	private double prev_error = 0; 
+	private double prev_val = Double.POSITIVE_INFINITY;
+
+	private double bias;
+	private double Kp;
+	private double Ki;
+	private double Kd;
+
+	private double state = 0;
+
+	public PIDController(double bias, double Kp, double Ki, double Kd) {
+		this.bias = bias;
+		this.Kp = Kp;
+		this.Ki = Ki;
+		this.Kd = Kd;
+	}
+
+	protected double getLastResponse() {
+		return this.state;
+	}
+
+	// Calculates PID value 
+	protected double response(double setpoint, double current_value, double delta_time)  {
+		double error = setpoint - current_value;
+		if (this.prev_error == Double.POSITIVE_INFINITY) {
+			this.prev_error = error;
+		}
+		this.commulating_error += error;
+		double proportional = this.Kp * error;
+    	double integral = this.Ki * this.commulating_error * delta_time;
+    	double derivative = this.Kd * (error - this.prev_error) / delta_time;
+		this.prev_error = error;
+		return proportional + integral + derivative;
+	}
+}
