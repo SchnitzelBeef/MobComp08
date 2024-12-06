66f95b0d4d992ef150a817d02c573f44a8ebfda8
rkaundal00 <rahulkaundal00@hotmail.com>
2024-12-06 12:32:45 +0100

    Added code for estimating throughput

--- TitTatBlaster3000.java
+++ TitTatBlaster3000.java
@@ -16,6 +16,13 @@ public class TitTatBlaster3000 extends JE802_11MacAlgorithm {
 
 	private boolean explore = true;
 
+	// Fields for estimating the throughput
+	private int previous_queue_state;
+	private int measurement_periode;
+	private int current_measurement_period;
+	private int packet_sent;
+	private double current_throughput;
+
 	public TitTatBlaster3000(String name, JE802_11Mac mac) {
 		super(name, mac);
 		this.theBackoffEntityAC01 = this.mac.getBackoffEntity(1);
@@ -25,6 +32,12 @@ public class TitTatBlaster3000 extends JE802_11MacAlgorithm {
 		theBackoffEntityAC01.setDot11EDCAAIFSN(2);
 		theBackoffEntityAC01.setDot11EDCACWmin(15);
 		this.pid_controller = new PIDController(-0.001, 0.001, 0.002, 0.0001);
+
+		this.previous_queue_state = 0;
+		this.measurement_periode = 10; // After how many calls of compute() the throughput should be calculated
+		this.current_measurement_period = 0;
+		this.packet_sent = 0;
+		this.current_throughput = 0; // FIXME: Change th initial value to something reasonable
 	}
 
 
@@ -63,6 +76,21 @@ public class TitTatBlaster3000 extends JE802_11MacAlgorithm {
 		
 		Integer AIFSN_AC01 = theBackoffEntityAC01.getDot11EDCAAIFSN();
 		Integer CWmin_AC01 = theBackoffEntityAC01.getDot11EDCACWmin();
+
+		// Here we calculate the throughput based on how the queue size changes
+		// If the queue size was 1 in the previous state and now it is 0, we assume that a packet was sent
+		if (this.previous_queue_state == 1 && aCurrentQueueSize == 0) {
+			this.packet_sent++;
+		}
+
+		if (this.current_measurement_period > this.measurement_periode) {
+			this.current_throughput = this.packet_sent / (this.measurement_periode * this.theSamplingTime_sec);
+			this.packet_sent = 0;
+			this.current_measurement_period = 0;
+			message("Throughput: " + this.current_throughput, 100);
+		}
+
+		this.current_measurement_period++;
 
 		if (this.pid_controller.getLastResponse() <= 0.5) {
 			this.cooperate(AIFSN_AC01, CWmin_AC01);
